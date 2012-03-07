427ceaa24494a707b5e885a6163935df3fc8c709
Yasuhito Takamiya <yasuhito@gmail.com>
2012-03-07 15:03:27 +0900

    Updated.

--- packetin-dumper.rb
+++ packetin-dumper.rb
@@ -1,7 +1,7 @@
 class PacketinDumper < Controller
-  def packet_in dpid, event
+  def packet_in dpid, message
     puts "received a packet_in"
     info "dpid: #{ dpid.to_hex }"
-    info "in_port: #{ event.in_port }"
+    info "in_port: #{ message.in_port }"
   end
 end
